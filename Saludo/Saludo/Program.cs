6c7f225c66a3ca101d9d05f0ef18f6168adacdfc
saulocervantes <saulo.cervantes@hotmail.com>
2016-11-22 23:38:02 -0600

    Anexo el proyecto Saludo de la sección 2 del curso.

---
+++ Saludo/Saludo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Saludo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola " + args[0]);
+        }
+    }
+}
