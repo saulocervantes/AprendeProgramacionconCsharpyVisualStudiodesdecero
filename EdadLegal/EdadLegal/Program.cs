99f977d41e2afa1a5662a1ab9da84478d3651c73
saulocervantes <saulo.cervantes@hotmail.com>
2016-11-22 22:36:51 -0600

    Carga del avance del curso
    Programa de edad legal

---
+++ EdadLegal/EdadLegal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EdadLegal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su nombre y pulse Enter");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Escriba su edad y pulse Enter");
+            int edad = Convert.ToInt32( Console.ReadLine() );
+
+            if ( edad >= 18 )
+            {
+                Console.WriteLine("Tu nombre es {0} y legalmente eres mayor de edad.", nombre);
+            }
+            else
+            {
+                Console.WriteLine("Tu nombre es {0} y legalmente no eres mayor de edad", nombre);
+            }
+
+        }
+    }
+}
