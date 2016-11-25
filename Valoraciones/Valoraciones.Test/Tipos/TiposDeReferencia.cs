1f5c84895d1051bbba3ee8cc353f37a57a112df1
saulocervantes <saulo.cervantes@hotmail.com>
2016-11-24 22:08:12 -0600

    35 Variables de Tipo Valor, dejamos un test fallido donde comprobamos las variabes de tipo por referencia.

--- Valoraciones/Valoraciones.Test/Tipos/TiposDeReferencia.cs
+++ Valoraciones/Valoraciones.Test/Tipos/TiposDeReferencia.cs
@@ -18,6 +18,10 @@ namespace Valoraciones.Test.Tipos
 
             LibroValoraciones l2 = l1;
 
+            l1.Nombre = "Libro ...";
+
+            l1 = new LibroValoraciones();
+
             Assert.AreEqual(l1.Nombre, l2.Nombre);
         }
     }
