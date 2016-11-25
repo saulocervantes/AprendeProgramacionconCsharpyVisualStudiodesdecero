b627e6882847b0f68766c4fe0d4d58a44fd9414e
saulocervantes <saulo.cervantes@hotmail.com>
2016-11-25 01:02:35 -0600

    38. Inmutabilidad Se comprobo la inmutabilidad de las variables al crear una clase de pruebas, con sus respectivos TestMethods.

---
+++ Valoraciones/Valoraciones.Test/ImmutableTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valoraciones.Test
+{
+    [TestClass]
+    public class ImmutableTest
+    {
+        [TestMethod]
+        public void ImmutableTestMetodo()
+        {
+            string nombre = "Mariano";
+            nombre = nombre.ToUpper();
+
+            Assert.AreEqual("MARIANO",nombre);
+
+            LibroValoraciones libro1 = new LibroValoraciones();
+            libro1.Nombre = "Mariano";
+            libro1.Nombre = libro1.Nombre.ToUpper();
+
+            Assert.AreEqual("MARIANO",libro1.Nombre);
+
+            DateTime fecha = new DateTime(1997,8,26);
+            fecha = fecha.AddDays(1);
+
+            Assert.AreEqual(27, fecha.Day);
+        }
+    }
+}
